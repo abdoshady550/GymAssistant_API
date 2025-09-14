353b095ef8136215ce47c081e7aefb9e52593696
abdoshady <abdos@KAKASHI>
2025-09-14 14:15:11 +0300

    Add AppDbContext with entity configurations and precision
    This commit introduces a new `AppDbContext` class that inherits from `IdentityDbContext<AppUser>`, enabling identity management. It includes DbSet properties for various entities such as `ClientProfile`, `BodyMeasurement`, `TrainerTrainee`, `WorkoutSession`, `WorkoutExercise`, `ExerciseSet`, `Exercise`, `UserExercise`, `Section`, and `PersonalRecord`. The `OnModelCreating` method is overridden to apply configurations from the assembly, and a new method `ConfigureConventions` is added to set precision for decimal properties.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+using GymAssistant_API.Model.Entities.Exercise;
+using GymAssistant_API.Model.Entities.User;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymAssistant_API.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        // DbSets
+        public DbSet<ClientProfile> ClientProfiles => Set<ClientProfile>();
+        public DbSet<BodyMeasurement> BodyMeasurements => Set<BodyMeasurement>();
+        public DbSet<TrainerTrainee> TrainerTrainees => Set<TrainerTrainee>();
+        public DbSet<WorkoutSession> WorkoutSessions => Set<WorkoutSession>();
+        public DbSet<WorkoutExercise> WorkoutExercises => Set<WorkoutExercise>();
+        public DbSet<ExerciseSet> ExerciseSets => Set<ExerciseSet>();
+        public DbSet<Exercise> Exercises => Set<Exercise>();
+        public DbSet<UserExercise> UserExercises => Set<UserExercise>();
+        public DbSet<Section> Sections => Set<Section>();
+        public DbSet<PersonalRecord> PersonalRecords => Set<PersonalRecord>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+            // Add the seeding
+
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            configurationBuilder.Properties<decimal>()
+                .HavePrecision(10, 2);
+        }
+    }
+}
