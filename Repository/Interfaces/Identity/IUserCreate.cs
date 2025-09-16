0fe6f3f8e9cc40b323cecbc93f27f17d970cde60
abdoshady <abdos@KAKASHI>
2025-09-16 12:21:23 +0300

    feat(identity): add IUserCreate interface for user registration
    - Defined `AddUserAsync` method to handle user creation using `RegisterRequest`.
    - Returns a `Result<AppUserDto>` for consistent success/error handling.

---
+++ Repository/Interfaces/Identity/IUserCreate.cs
@@ -0,0 +1,11 @@
+﻿using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Req_Res.Reqeust;
+
+namespace GymAssistant_API.Repository.Interfaces.Identity
+{
+    public interface IUserCreate
+    {
+        Task<Result<AppUserDto>> AddUserAsync(RegisterRequest request, CancellationToken ct = default);
+    }
+}
