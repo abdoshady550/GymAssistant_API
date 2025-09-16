5ee01b505ad7b784809e0c5cf8584c1baace0844
abdoshady <abdos@KAKASHI>
2025-09-16 12:13:29 +0300

    Add RegisterHandler for user registration handling
    This commit introduces a new `RegisterHandler` class in the
    `GymAssistant_API.Handeler.Identity` namespace. The class
    facilitates user registration by utilizing dependency injection
    for logging and user creation. The `Handle` method processes
    `RegisterRequest`, manages user creation, logs errors, and
    returns either the created user's data or error details.

---
+++ Handeler/Identity/RegisterHandler.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Core;
+using GymAssistant_API.Data;
+using GymAssistant_API.Model.Entities.User;
+using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Repository.Interfaces.Identity;
+using GymAssistant_API.Req_Res.Reqeust;
+using Microsoft.AspNetCore.Identity;
+
+namespace GymAssistant_API.Handeler.Identity
+{
+    public class RegisterHandler(
+    ILogger<ApplicationDbContextInitialiser> logger,
+   IUserCreate userCreate)
+    {
+        private readonly ILogger<ApplicationDbContextInitialiser> _logger = logger;
+        private readonly IUserCreate _userCreate = userCreate;
+
+
+        public async Task<Result<AppUserDto>> Handle(RegisterRequest Request, CancellationToken ct)
+        {
+            var createUser = await _userCreate.AddUserAsync(Request, ct);
+
+            if (createUser.IsError)
+            {
+                _logger.LogError("User with UserName { UserName }{ErrorDetails}", Request.UserName, createUser.TopError.Description);
+                return createUser.Errors;
+            }
+
+            return createUser.Value;
+
+
+
+
+        }
+
+
+
+
+    }
+}
+
