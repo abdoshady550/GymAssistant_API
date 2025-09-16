89da8cf0e6d4114d86cc1996bbcec0ac2658a702
abdoshady <abdos@KAKASHI>
2025-09-16 12:12:42 +0300

    Add GenerateTokenQueryHandler for JWT token generation
    Introduces the `GenerateTokenQueryHandler` class in the
    `GymAssistant_API.Handeler.Identity` namespace. This class
    handles user authentication and JWT token generation,
    utilizing injected dependencies for logging, identity services,
    and token management. The `Handle` method processes
    `LoginRequest` objects, authenticates users, and generates
    tokens while implementing error handling for both
    authentication and token generation failures.

---
+++ Handeler/Identity/GenerateTokenQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Repository.Interfaces.Identity;
+using GymAssistant_API.Req_Res.Reqeust;
+
+namespace GymAssistant_API.Handeler.Identity
+{
+    public class GenerateTokenQueryHandler(ILogger<GenerateTokenQueryHandler> logger, IIdentityService identityService, ITokenProvider tokenProvider)
+    {
+        private readonly ILogger<GenerateTokenQueryHandler> _logger = logger;
+        private readonly IIdentityService _identityService = identityService;
+        private readonly ITokenProvider _tokenProvider = tokenProvider;
+
+        public async Task<Result<TokenResponse>> Handle(LoginRequest query, CancellationToken ct)
+        {
+            var userResponse = await _identityService.AuthenticateAsync(query.Email, query.Password);
+
+            if (userResponse.IsError)
+            {
+                return userResponse.Errors;
+            }
+            var generateTokenResult = await _tokenProvider.GenerateJwtTokenAsync(userResponse.Value, ct);
+
+            if (generateTokenResult.IsError)
+            {
+                _logger.LogError("Generate token error occurred: {ErrorDescription}", generateTokenResult.TopError.Description);
+
+                return generateTokenResult.Errors;
+            }
+
+            return generateTokenResult.Value;
+        }
+    }
+}
