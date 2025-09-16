fbdd94af51d50dff4dce9941d656bace4f6d53d4
abdoshady <abdos@KAKASHI>
2025-09-16 12:25:05 +0300

    feat(identity): add INotificationService interface for email & SMS delivery
    - Defined `SendEmailAsync` and `SendSmsAsync` methods for sending notifications.
    - Provides a contract for implementing email and SMS notification services.

---
+++ Repository/Interfaces/Identity/INotificationService.cs
@@ -0,0 +1,7 @@
+﻿namespace GymAssistant_API.Repository.Interfaces.Identity;
+public interface INotificationService
+{
+    Task SendEmailAsync(string to, CancellationToken cancellationToken = default);
+
+    Task SendSmsAsync(string phoneNumber, CancellationToken cancellationToken = default);
+}
