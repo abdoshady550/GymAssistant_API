7a177f1029f823edfab797195116411d79e04afb
abdoshady <abdos@KAKASHI>
2025-09-16 12:20:48 +0300

    feat(identity): add UtilityService with email masking helper
    - Introduced `MaskEmail` method to obfuscate sensitive parts of an email.
    - Ensures privacy by replacing characters between the first and last character before '@' with "****".

---
+++ Repository/Interfaces/Identity/UtilityService.cs
@@ -0,0 +1,15 @@
+namespace GymAssistant_API.Repository.Interfaces.Identity;
+
+public static class UtilityService
+{
+    public static string MaskEmail(string email)
+    {
+        int atIndex = email.IndexOf('@');
+        if (atIndex <= 1)
+        {
+            return $"****{email.AsSpan(atIndex)}";
+        }
+
+        return email[0] + "****" + email[atIndex - 1] + email[atIndex..];
+    }
+}
