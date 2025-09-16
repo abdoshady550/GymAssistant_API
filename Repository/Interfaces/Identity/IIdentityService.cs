d0fdba01b8024fff0adf6c474d54a5284f7036c1
abdoshady <abdos@KAKASHI>
2025-09-16 12:20:15 +0300

    feat(identity): add IIdentityService interface for authentication & authorization
    - Defined methods for role checking, policy-based authorization, and user authentication.
    - Added support for retrieving user details (by ID, username) and returning results as `Result<AppUserDto>`.

---
+++ Repository/Interfaces/Identity/IIdentityService.cs
@@ -0,0 +1,17 @@
+using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+
+namespace GymAssistant_API.Repository.Interfaces.Identity;
+
+public interface IIdentityService
+{
+    Task<bool> IsInRoleAsync(string userId, string role);
+
+    Task<bool> AuthorizeAsync(string userId, string? policyName);
+
+    Task<Result<AppUserDto>> AuthenticateAsync(string email, string password);
+
+    Task<Result<AppUserDto>> GetUserByIdAsync(string userId);
+
+    Task<string?> GetUserNameAsync(string userId);
+}
