fc10d6106a106d079fc8d2e5b316ef374f7e7fab
abdoshady <abdos@KAKASHI>
2025-09-16 12:13:10 +0300

    Add RefreshTokenQueryHandler for token management
    Introduces the `RefreshTokenQueryHandler` class in the
    `GymAssistant_API.Handeler.Identity` namespace. This class
    handles refresh token requests by validating expired access
    tokens, retrieving user information, checking refresh token
    validity, and generating new JWT tokens. Includes error
    handling and logging for improved reliability.

---
+++ Handeler/Identity/RefreshTokenQueryHandler.cs
@@ -0,0 +1,64 @@
+﻿using GymAssistant_API.Data;
+using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Repository.Interfaces.Identity;
+using GymAssistant_API.Req_Res.Reqeust;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace GymAssistant_API.Handeler.Identity
+{
+    public class RefreshTokenQueryHandler(ILogger<RefreshTokenQueryHandler> logger, IIdentityService identityService, AppDbContext context, ITokenProvider tokenProvider)
+    {
+        private readonly ILogger<RefreshTokenQueryHandler> _logger = logger;
+        private readonly IIdentityService _identityService = identityService;
+        private readonly AppDbContext _context = context;
+        private readonly ITokenProvider _tokenProvider = tokenProvider;
+
+        public async Task<Result<TokenResponse>> Handle(RefreshTokenQuery request, CancellationToken ct)
+        {
+            var principal = _tokenProvider.GetPrincipalFromExpiredToken(request.ExpiredAccessToken);
+
+            if (principal is null)
+            {
+                _logger.LogError("Expired access token is not valid");
+
+                return ApplicationErrors.ExpiredAccessTokenInvalid;
+            }
+            var userId = principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userId is null)
+            {
+                _logger.LogError("Invalid userId claim");
+
+                return ApplicationErrors.UserIdClaimInvalid;
+            }
+            var getUserResult = await _identityService.GetUserByIdAsync(userId);
+
+            if (getUserResult.IsError)
+            {
+                _logger.LogError("Get user by id error occurred: {ErrorDescription}", getUserResult.TopError.Description);
+                return getUserResult.Errors;
+            }
+            var refreshToken = await _context.RefreshTokens.FirstOrDefaultAsync(r => r.Token == request.RefreshToken && r.UserId == userId, ct);
+
+            if (refreshToken is null || refreshToken.ExpiresOnUtc < DateTime.UtcNow)
+            {
+                _logger.LogError("Refresh token has expired");
+
+                return ApplicationErrors.RefreshTokenExpired;
+            }
+
+            var generateTokenResult = await _tokenProvider.GenerateJwtTokenAsync(getUserResult.Value, ct);
+
+            if (generateTokenResult.IsError)
+            {
+                _logger.LogError("Generate token error occurred: {ErrorDescription}", generateTokenResult.TopError.Description);
+
+                return generateTokenResult.Errors;
+            }
+
+            return generateTokenResult.Value;
+        }
+    }
+}
