e6eee81cd6104ea8d64dadc36feebd39008150b2
abdoshady <abdos@KAKASHI>
2025-09-16 12:12:57 +0300

    Add GetUserByIdQueryHandler for user retrieval
    Introduces the `GetUserByIdQueryHanlder` class in the
    `GymAssistant_API.Handeler.Identity` namespace to handle
    user information retrieval by user ID. The class includes
    a constructor for dependency injection of logging and
    identity service, and an asynchronous `Handle` method
    that retrieves user details, logs errors, and returns
    the user data or error details as appropriate.

---
+++ Handeler/Identity/GetUserByIdQueryHanlder.cs
@@ -0,0 +1,28 @@
+﻿using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Repository.Interfaces.Identity;
+using GymAssistant_API.Req_Res.Reqeust;
+
+namespace GymAssistant_API.Handeler.Identity
+{
+    public class GetUserByIdQueryHanlder(ILogger<GetUserByIdQueryHanlder> logger,
+                                         IIdentityService identityService)
+
+    {
+        private readonly ILogger<GetUserByIdQueryHanlder> _logger = logger;
+        private readonly IIdentityService _identityService = identityService;
+        public async Task<Result<AppUserDto>> Handle(GetUserByIdQuery request, CancellationToken ct)
+        {
+            var getUserByIdResult = await _identityService.GetUserByIdAsync(request.UserId!);
+
+            if (getUserByIdResult.IsError)
+            {
+                _logger.LogError("User with Id { UserId }{ErrorDetails}", request.UserId, getUserByIdResult.TopError.Description);
+
+                return getUserByIdResult.Errors;
+            }
+
+            return getUserByIdResult.Value;
+        }
+    }
+}
