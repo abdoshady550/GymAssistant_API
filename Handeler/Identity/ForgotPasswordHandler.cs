e621726de7a21d98ba0b183c4dc655107eb3c92e
abdoshady <abdos@KAKASHI>
2025-09-19 10:17:15 +0300

    feat(identity): add ForgotPasswordHandler to handle forgot password requests

---
+++ Handeler/Identity/ForgotPasswordHandler.cs
@@ -0,0 +1,33 @@
+﻿using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Repository.Interfaces.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace GymAssistant_API.Handeler.Identity
+{
+    public class ForgotPasswordHandler(ILogger<ForgotPasswordHandler> logger,
+        IIdentityService identityService)
+    {
+        private readonly IIdentityService _identityService = identityService;
+        private readonly ILogger<ForgotPasswordHandler> _logger = logger;
+
+
+
+        public async Task<Result<string>> Handle([FromBody] ForgotPasswordDto request, CancellationToken ct = default)
+        {
+
+
+            var result = await _identityService.ForgotPasswordAsync(request.Email);
+
+            if (result.IsError)
+            {
+                _logger.LogError("User with Email { Email }{ErrorDetails}", request.Email, result.TopError.Description);
+
+                return Error.NotFound("Invalid email", result.TopError.Description);
+            }
+            return result.Value;
+        }
+
+    }
+}
