1ed8f3387662914047b0e4cba068c89d8b765e08
abdoshady <abdos@KAKASHI>
2025-09-19 10:04:45 +0300

    feat(config): add EF Core configuration for PasswordResetToken
    Configured PasswordResetToken entity with:
    
    Primary key on Id
    
    Required Email and Token with max length 255
    
    Unique index on Token
    
    Composite index on (Email, Token)
    
    Improves query performance and ensures token uniqueness

---
+++ Data/Configurations/User/PasswordResetTokenConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using GymAssistant_API.Model.Entities;
+using GymAssistant_API.Model.Entities.User;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymAssistant_API.Data.Configurations.User
+{
+    public class PasswordResetTokenConfiguration : IEntityTypeConfiguration<PasswordResetToken>
+    {
+        public void Configure(EntityTypeBuilder<PasswordResetToken> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Email).IsRequired().HasMaxLength(255);
+            builder.Property(e => e.Token).IsRequired().HasMaxLength(255);
+            builder.HasIndex(e => e.Token).IsUnique();
+            builder.HasIndex(e => new { e.Email, e.Token });
+        }
+    }
+}
