b10bd9533f98a0d83fb83552645a20c5560231b0
abdoshady <abdos@KAKASHI>
2025-09-19 10:11:34 +0300

    feat(entity): add PasswordResetToken entity for password reset flow
    Added PasswordResetToken entity with properties:
    
    Email, Token, ExpiryDate, IsUsed
    
    Integrated with Entity base class for Id and audit fields
    
    Supports storing and tracking password reset requests in DB

---
+++ Model/Entities/User/PasswordResetToken.cs
@@ -0,0 +1,13 @@
+﻿namespace GymAssistant_API.Model.Entities.User
+{
+    public class PasswordResetToken : Entity
+    {
+
+        public string Email { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public DateTime ExpiryDate { get; set; }
+        public bool IsUsed { get; set; } = false;
+
+
+    }
+}
