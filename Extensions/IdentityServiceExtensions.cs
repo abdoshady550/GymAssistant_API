55365aa39ba720d48a12fcdc3fabce2e521a9ab0
abdoshady <abdos@KAKASHI>
2025-09-20 17:19:15 +0300

    Add IdentityServiceExtensions to convert IdentityErrors into a list of validation Errors

---
+++ Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GymAssistant_API.Model.Results;
+using Microsoft.AspNetCore.Identity;
+
+namespace GymAssistant_API.Extensions
+{
+    public class IdentityServiceExtensions
+    {
+        public static List<Error> ConvertIdentityErrors(IEnumerable<IdentityError> identityErrors)
+        {
+            return identityErrors.Select(error => Error.Validation(error.Code, error.Description)).ToList();
+        }
+    }
+}
