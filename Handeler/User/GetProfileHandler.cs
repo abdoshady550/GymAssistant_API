b3b41de26b90de32a26db5cacfac32af5f2f5483
abdoshady <abdos@KAKASHI>
2025-09-22 11:41:08 +0300

    🐛 Fix GetProfileHandler by renaming private fields (`_logger`, `_profile`) to avoid shadowing constructor parameters and ensure proper dependency usage.

---
+++ Handeler/User/GetProfileHandler.cs
@@ -0,0 +1,24 @@
+﻿using GymAssistant_API.Model.Results;
+using GymAssistant_API.Repository.Interfaces.User;
+using GymAssistant_API.Req_Res.Response;
+
+namespace GymAssistant_API.Handeler.User
+{
+    public sealed class GetProfileHandler(ILogger<GetProfileHandler> logger,
+                                   IProfile profile)
+    {
+        private readonly ILogger<GetProfileHandler> logger = logger;
+        private readonly IProfile profile = profile;
+        public async Task<Result<ProfileResponse>> Handle(string Id, CancellationToken ct = default)
+        {
+            var getProfile = await profile.GetProfileAsync(Id, ct);
+            if (getProfile.IsError)
+            {
+                logger.LogError("Failed to get profile for user {UserId}: {Error}", Id, getProfile.Errors);
+                return getProfile.Errors;
+            }
+            return getProfile;
+
+        }
+    }
+}
