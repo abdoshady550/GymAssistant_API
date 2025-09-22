5582bd8241b4cedd6b77cfff78ceb17d45fd46ac
abdoshady <abdos@KAKASHI>
2025-09-22 11:49:44 +0300

    Create ProfileResponse DTO to return user profile details including personal info, role, and body measurements

---
+++ Req&Res/Response/ProfileResponse.cs
@@ -0,0 +1,19 @@
+﻿using GymAssistant_API.Model.Entities.User;
+using System.Runtime.CompilerServices;
+
+namespace GymAssistant_API.Req_Res.Response
+{
+    public class ProfileResponse
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public Gender? Gender { get; set; } = default;
+        public UserRole? Role { get; set; } = default;
+        public int? HeightCm { get; set; } = default;
+        public decimal? WeightKg { get; set; } = default;
+        public decimal? MuscleMassKgdecimal { get; set; } = default;
+        public decimal? BodyFatPercent { get; set; } = default;
+
+    }
+}
