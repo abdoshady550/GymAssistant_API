5607852018297e19b9bc6c242794eb0f51c953fc
abdoshady <abdos@KAKASHI>
2025-09-23 13:52:40 +0300

    feat: add BodyMeasurementDto record to represent user body measurement data

---
+++ Model/Entities/User/Dto/BodyMeasurementDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GymAssistant_API.Model.Entities.User.Dto
+{
+    public record BodyMeasurementDto(
+     Guid? Id = default,
+     decimal? WeightKg = default,
+     decimal? MuscleMassKg = default,
+     decimal? BodyFatPercent = default,
+     DateTimeOffset? CreatedAtUtc = default);
+
+}
