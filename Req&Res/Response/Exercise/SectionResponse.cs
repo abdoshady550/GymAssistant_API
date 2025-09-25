22e797160902bc28101920ee6470614accea82d5
abdoshady <abdos@KAKASHI>
2025-09-25 11:30:04 +0300

    feat(response): introduce `SectionResponse` record to represent exercise section data in API responses

---
+++ Req&Res/Response/Exercise/SectionResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Exercise
+{
+    public record SectionResponse(
+       Guid Id,
+       string Name,
+       string? Description = null,
+       DateTimeOffset? CreatedAtUtc = null
+   );
+}
