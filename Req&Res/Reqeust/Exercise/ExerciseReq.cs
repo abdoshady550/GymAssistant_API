c2cbd7a3cdc0b70fda5c50f8c1cf766d27e99afb
abdoshady <abdos@KAKASHI>
2025-09-25 11:28:58 +0300

    fix(exercise-req): align `UpdateExerciseReq` with `ExerciseReq` by making `Name` required and providing default values for optional fields

---
+++ Req&Res/Reqeust/Exercise/ExerciseReq.cs
@@ -0,0 +1,26 @@
+﻿using GymAssistant_API.Model.Entities.Exercise;
+
+namespace GymAssistant_API.Req_Res.Reqeust.Exercise
+{
+    public record ExerciseReq(
+      string Name,
+      string? Description = null,
+      string? Instructions = null,
+      IFormFile? ImageFile = null,
+      string? Equipment = null,
+      DifficultyLevel? DifficultyLevel = null,
+      int? DefaultSets = null,
+      int? DefaultReps = null
+  );
+    public record UpdateExerciseReq(
+    string? Name,
+    string? Description,
+    string? Instructions,
+    IFormFile? ImageFile,
+    string? Equipment,
+    DifficultyLevel? DifficultyLevel,
+    int? DefaultSets,
+    int? DefaultReps
+ );
+
+}
