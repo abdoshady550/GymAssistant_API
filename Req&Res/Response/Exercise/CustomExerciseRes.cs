6926204deb6f1a61453033126ad73c167669569c
abdoshady <abdos@KAKASHI>
2025-09-25 11:29:20 +0300

    feat(response): add `CustomExerciseRes` record for consistent DTO representation of user-created exercises

---
+++ Req&Res/Response/Exercise/CustomExerciseRes.cs
@@ -0,0 +1,13 @@
+﻿namespace GymAssistant_API.Req_Res.Response
+{
+    public record CustomExerciseRes(
+      Guid Id,
+      string UserId,
+      string Name,
+      string? Description = default,
+      string? Instructions = default,
+      string? Equipment = default,
+      string? ImageUrl = default,
+      DateTimeOffset? CreatedAtUtc = default
+  );
+}
