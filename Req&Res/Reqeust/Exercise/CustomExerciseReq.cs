7d392b02806b9c71b9e09245f073f4456a1ff596
abdoshady <abdos@KAKASHI>
2025-09-25 11:28:06 +0300

    fix(custom-exercise-req): rename `ImageUrl` to `ImageFile` and use `IFormFile` to match service signature

---
+++ Req&Res/Reqeust/Exercise/CustomExerciseReq.cs
@@ -0,0 +1,11 @@
+﻿namespace GymAssistant_API.Req_Res.Reqeust.Exercise
+{
+    public record class CustomExerciseReq(
+        string Name,
+        string? Description = default,
+        string? Instructions = default,
+        string? Equipment = default,
+        IFormFile? ImageUrl = default
+    );
+
+}
