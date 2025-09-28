f04acd463c31633ffb5e56225e942901e68a3441
abdoshady <abdos@KAKASHI>
2025-09-28 09:27:26 +0300

    feat(api): add CreateWorkoutSessionRequest record with date, optional notes, and trainee ID for session creation

---
+++ Req&Res/Reqeust/Exercise/CreateWorkoutSessionRequest.cs
@@ -0,0 +1,10 @@
+﻿
+namespace GymAssistant_API.Req_Res.Reqeust.Exercises
+{
+    public record CreateWorkoutSessionRequest(
+
+        DateTime Date,
+        string? Notes = null,
+        Guid? TraineeId = null);
+
+}
