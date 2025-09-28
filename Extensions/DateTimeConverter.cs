16a95664ed815f53b82c029f2996bcb8f30d9187
abdoshady <abdos@KAKASHI>
2025-09-28 09:20:26 +0300

    feat(json): add custom DateTime converter supporting multiple date formats for consistent serialization/deserialization

---
+++ Extensions/DateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GymAssistant_API.Extensions
+{
+    public class DateTimeConverter : JsonConverter<DateTime>
+    {
+        private static readonly string[] Formats =
+            { "yyyy-MM-dd", "yyyy-MM-ddTHH:mm:ss", "yyyy/MM/dd", "yyyy/MM/ddTHH:mm:ss" };
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var value = reader.GetString();
+            if (DateTime.TryParseExact(value, Formats, CultureInfo.InvariantCulture, DateTimeStyles.None, out var date))
+            {
+                return date;
+            }
+            throw new JsonException($"Invalid date format: {value}");
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString("yyyy-MM-ddTHH:mm:ss"));
+        }
+    }
+}
