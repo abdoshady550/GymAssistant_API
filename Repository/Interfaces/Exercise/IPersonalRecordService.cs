8aee338a25527699f68fb43069126f9c8fcd84bc
abdoshady <abdos@KAKASHI>
2025-09-28 09:21:55 +0300

    feat(records): add IPersonalRecordService interface for checking and retrieving personal records

---
+++ Repository/Interfaces/Exercise/IPersonalRecordService.cs
@@ -0,0 +1,11 @@
+﻿using GymAssistant_API.Model.Entities.Exercise;
+
+namespace GymAssistant_API.Repository.Interfaces.Exercises
+{
+    public interface IPersonalRecordService
+    {
+        Task CheckAndCreatePersonalRecordsAsync(WorkoutSession session, CancellationToken ct = default);
+        Task<List<PersonalRecord>> GetPersonalRecordsAsync(string userId, RecordType? recordType = null, CancellationToken ct = default);
+    }
+
+}
