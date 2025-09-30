07b62a71663d96ea9049fbe3adf018ae55d4438c
abdoshady <abdos@KAKASHI>
2025-09-30 23:13:55 +0300

    feat(trainer): add TraineeProgressData DTO aggregating trainee info, progress overview, records, workouts, and section stats

---
+++ Req&Res/Response/Trainer/TraineeProgressData.cs
@@ -0,0 +1,16 @@
+﻿using GymAssistant_API.Model.Entities.Exercise;
+using GymAssistant_API.Req_Res.Response.Exercise;
+using GymAssistant_API.Req_Res.Response.Progress;
+using GymAssistant_API.Req_Res.Response.Records;
+
+namespace GymAssistant_API.Req_Res.Response.Trainer
+{
+    public class TraineeProgressData
+    {
+        public TraineeData TraineeInfo { get; set; } = new();
+        public ProgressOverviewData ProgressOverview { get; set; } = new();
+        public List<PersonalRecordResponse> RecentRecords { get; set; } = new();
+        public List<WorkoutSessionRes> RecentWorkouts { get; set; } = new();
+        public List<SectionProgressData> SectionProgress { get; set; } = new();
+    }
+}
