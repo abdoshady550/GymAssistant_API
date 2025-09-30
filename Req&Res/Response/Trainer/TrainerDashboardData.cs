40e3e7014b61fc82b160754356d4f26feba8e35f
abdoshady <abdos@KAKASHI>
2025-09-30 23:14:11 +0300

    feat(trainer): add TrainerDashboardData DTO with trainees, sessions, and activity insights for dashboard use

---
+++ Req&Res/Response/Trainer/TrainerDashboardData.cs
@@ -0,0 +1,16 @@
+﻿using GymAssistant_API.Model.Entities.Exercise;
+using GymAssistant_API.Req_Res.Response.Exercise;
+
+namespace GymAssistant_API.Req_Res.Response.Trainer
+{
+    public class TrainerDashboardData
+    {
+        public int TotalTrainees { get; set; }
+        public int ActiveTraineesToday { get; set; }
+        public int TotalSessionsCreated { get; set; }
+        public int SessionsThisWeek { get; set; }
+        public List<TraineeData> RecentlyActiveTrainees { get; set; } = new();
+        public List<WorkoutSessionRes> RecentSessions { get; set; } = new();
+        public Dictionary<string, int> TraineesBySection { get; set; } = new();
+    }
+}
