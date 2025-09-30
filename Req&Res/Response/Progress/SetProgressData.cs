d8d59ee3329eea2a53900990c2e33b459e0e6187
abdoshady <abdos@KAKASHI>
2025-09-30 23:11:49 +0300

    feat(progress): add SetProgressData model with set details, volume, and PR flag

---
+++ Req&Res/Response/Progress/SetProgressData.cs
@@ -0,0 +1,11 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Progress
+{
+    public class SetProgressData
+    {
+        public int SetNumber { get; set; }
+        public int Reps { get; set; }
+        public decimal WeightKg { get; set; }
+        public decimal Volume { get; set; }
+        public bool IsPersonalRecord { get; set; }
+    }
+}
