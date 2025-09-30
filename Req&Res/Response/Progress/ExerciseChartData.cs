87394eccbc47f94b294e8b8f9726b39c90f3ee01
abdoshady <abdos@KAKASHI>
2025-09-30 23:10:02 +0300

    feat(progress): add ExerciseChartData model to hold weight, volume, and reps progression chart points

---
+++ Req&Res/Response/Progress/ExerciseChartData.cs
@@ -0,0 +1,9 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Progress
+{
+    public class ExerciseChartData
+    {
+        public List<ChartPoint> WeightProgression { get; set; } = new();
+        public List<ChartPoint> VolumeProgression { get; set; } = new();
+        public List<ChartPoint> RepsProgression { get; set; } = new();
+    }
+}
