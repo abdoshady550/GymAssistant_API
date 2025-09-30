6ce11ceabd568c586f8ef19b51964cd03930623a
abdoshady <abdos@KAKASHI>
2025-09-30 23:12:43 +0300

    feat(records): add Milestone model with title, description, icon, and date fields

---
+++ Req&Res/Response/Records/Milestone.cs
@@ -0,0 +1,10 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Records
+{
+    public class Milestone
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public DateTimeOffset Date { get; set; }
+    }
+}
