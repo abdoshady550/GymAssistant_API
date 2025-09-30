bc2a318df1e2cbebea03a8cd69f8731a5d118c7f
abdoshady <abdos@KAKASHI>
2025-09-30 23:12:05 +0300

    feat(progress): add VolumeChartData model with daily/weekly trends and aggregate stats

---
+++ Req&Res/Response/Progress/VolumeChartData.cs
@@ -0,0 +1,10 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Progress
+{
+    public class VolumeChartData
+    {
+        public List<ChartPoint> DailyVolume { get; set; } = new();
+        public List<ChartPoint> WeeklyVolume { get; set; } = new();
+        public decimal TotalVolume { get; set; }
+        public decimal AverageDaily { get; set; }
+    }
+}
