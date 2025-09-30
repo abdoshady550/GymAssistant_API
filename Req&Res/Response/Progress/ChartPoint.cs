7a7196cae743cd139d49f9d004f23510da5ef71b
abdoshady <abdos@KAKASHI>
2025-09-30 23:09:40 +0300

    feat(progress): add ChartPoint model for representing date-value pairs in progress charts

---
+++ Req&Res/Response/Progress/ChartPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Progress
+{
+    public class ChartPoint
+    {
+        public DateTime Date { get; set; }
+        public decimal Value { get; set; }
+    }
+}
