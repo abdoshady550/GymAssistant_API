6d959e13c66fccfea1740b4a5c2a1f32357f17b3
abdoshady <abdos@KAKASHI>
2025-09-30 23:02:49 +0300

    fix(migrations): clean invalid WorkoutExercise references and add foreign keys with SetNull and restrict behaviors

---
+++ Data/Migrations/20250928085647_WorkoutExercise-WorkoutSession-navigation.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAssistant_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkoutExerciseWorkoutSessionnavigation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // نظف البيانات الأول
+            migrationBuilder.Sql(@"
+            UPDATE WorkoutExercises 
+            SET ExerciseId = NULL 
+            WHERE ExerciseId IS NOT NULL 
+            AND ExerciseId NOT IN (SELECT Id FROM Exercises);
+            
+            UPDATE WorkoutExercises 
+            SET UserExerciseId = NULL 
+            WHERE UserExerciseId IS NOT NULL 
+            AND UserExerciseId NOT IN (SELECT Id FROM UserExercises);
+        ");
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkoutExercises_ExerciseId",
+                table: "WorkoutExercises",
+                column: "ExerciseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkoutExercises_UserExerciseId",
+                table: "WorkoutExercises",
+                column: "UserExerciseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutExercises_Exercises_ExerciseId",
+                table: "WorkoutExercises",
+                column: "ExerciseId",
+                principalTable: "Exercises",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WorkoutExercises_UserExercises_UserExerciseId",
+                table: "WorkoutExercises",
+                column: "UserExerciseId",
+                principalTable: "UserExercises",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutExercises_Exercises_ExerciseId",
+                table: "WorkoutExercises");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WorkoutExercises_UserExercises_UserExerciseId",
+                table: "WorkoutExercises");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkoutExercises_ExerciseId",
+                table: "WorkoutExercises");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WorkoutExercises_UserExerciseId",
+                table: "WorkoutExercises");
+        }
+    }
+}
