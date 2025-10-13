22d08b41e7416ccdcc231cc6961d7f9be95f44c1
abdoshady <abdos@KAKASHI>
2025-10-13 16:07:47 +0300

    Remove Claims property from AppUserDto
    The `Claims` property of type `IList<Claim>` was removed from the
    `AppUserDto` record definition. The record now includes only
    `UserId`, `Email`, and `Roles` properties. Additionally, the
    parameters were reformatted to improve readability by placing
    each property on a new line.

--- Model/Identity/Dtos/AppUserDto.cs
+++ Model/Identity/Dtos/AppUserDto.cs
@@ -2,5 +2,7 @@
 
 namespace GymAssistant_API.Model.Identity.Dtos
 {
-    public sealed record AppUserDto(string UserId, string Email, IList<string> Roles, IList<Claim> Claims);
+    public sealed record AppUserDto(string UserId,
+                                    string Email,
+                                    IList<string> Roles);
 }
