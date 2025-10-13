ddfe948e7259092c41655ee7d9e49570f6eb7a28
abdoshady <abdos@KAKASHI>
2025-10-13 16:04:22 +0300

    Add properties and validations to UserExercise class
    Added new properties: DifficultyLevel, IsCustomExercise,
    SectionId, Section, SectionGroupId, and SectionGroup to
    the UserExercise class. Updated the constructor to require
    sectionId and difficultyLevel. Enhanced the Create method
    to validate sectionId and accept difficultyLevel. Modified
    the Update method to allow updates to sectionId and
    difficultyLevel. Ensured IsCustomExercise is set to true
    during object creation.

--- Model/Entities/Exercise/UserExercise.cs
+++ Model/Entities/Exercise/UserExercise.cs
@@ -11,6 +11,13 @@ namespace GymAssistant_API.Model.Entities.Exercise
         public string? Instructions { get; private set; }
         public string? ImageUrl { get; private set; }
         public string? Equipment { get; private set; }
+        public DifficultyLevel? DifficultyLevel { get; private set; }
+
+        public bool IsCustomExercise { get; private set; }
+        public Guid SectionId { get; private set; }
+        public Section Section { get; private set; } = default!;
+        public Guid? SectionGroupId { get; private set; }
+        public SectionGroup? SectionGroup { get; private set; } = default!;
 
         public Guid ClientProfileId { get; set; }
         public ClientProfile User { get; set; } = default!;
@@ -19,51 +26,68 @@ namespace GymAssistant_API.Model.Entities.Exercise
 
         private UserExercise(Guid id,
                              string userId,
+                             Guid sectionId,
                              string name,
                              string? description = null,
                              string? instructions = null,
                              string? equipment = null,
-                             string? imageUrl = null) : base(id)
+                             string? imageUrl = null,
+                             DifficultyLevel? difficultyLevel = null) : base(id)
         {
             UserId = userId;
+            SectionId = sectionId;
             Name = name;
             Description = description;
             Instructions = instructions;
             Equipment = equipment;
             ImageUrl = imageUrl;
+            DifficultyLevel = difficultyLevel;
+
             CreatedAtUtc = DateTimeOffset.UtcNow;
+            IsCustomExercise = true;
         }
         public static Result<UserExercise> Create(Guid id,
                                                   string userId,
+                                                  Guid sectionId,
                                                   string name,
                                                   string? description = null,
                                                   string? Instructions = null,
                                                   string? Equipment = null,
-                                                  string? ImageUrl = null)
+                                                  string? ImageUrl = null,
+                                                  DifficultyLevel? difficultyLevel = null)
         {
             if (userId == null)
             {
                 return UserErrors.IdRequired;
 
             }
+            if (sectionId == Guid.Empty)
+            {
+                return ExerciseErrors.SectionIdRequired;
+            }
             if (string.IsNullOrEmpty(name))
             {
                 return UserErrors.NameRequired;
             }
-            return new UserExercise(id, userId, name, description, Instructions, Equipment, ImageUrl);
+            return new UserExercise(id, userId, sectionId, name, description, Instructions, Equipment, ImageUrl, difficultyLevel);
         }
-        public Result<Updated> Update(
+        public Result<Updated> Update(Guid? sectionId,
                                                   string name,
                                                   string? description = null,
                                                   string? instructions = null,
                                                   string? equipment = null,
-                                                  string? imageUrl = null)
+                                                  string? imageUrl = null,
+                                                  DifficultyLevel? difficultyLevel = null)
         {
+            if (sectionId.HasValue)
+                SectionId = sectionId.Value;
             Name = name;
             Description = description;
             Instructions = instructions;
             Equipment = equipment;
             ImageUrl = imageUrl;
+            DifficultyLevel = difficultyLevel;
+
 
             return Result.Updated;
         }
