a887b9fe53505c3462663c9b80030e96972c0d19
abdoshady <abdos@KAKASHI>
2025-10-13 16:08:01 +0300

    Enhance exercise and section group management API
    Added new methods for managing section groups, including creation, retrieval, updating, and deletion. Updated existing methods to include additional parameters such as `userId`, `sectionId`, and `DifficultyLevel` for better context and functionality. Removed outdated methods that lacked these enhancements. Improved the `GetExercisesBySectionAsync` method to support search functionality and return a more comprehensive response type.

--- Repository/Interfaces/Exercise/IExercise.cs
+++ Repository/Interfaces/Exercise/IExercise.cs
@@ -8,15 +8,23 @@ namespace GymAssistant_API.Repository.Interfaces.ExerciseExercises
     public interface IExercise
     {
         Task<Result<List<SectionResponse>>> GetSectionsAsync(CancellationToken ct = default);
+        Task<Result<SectionResponse>> GetSectionByIdAsync(string userId, Guid sectionId, CancellationToken ct = default);
+        Task<Result<SectionGroupResponse>> CreateSectionGroup(string userId, Guid sectionId, string name, string descripion, CancellationToken ct = default);
+        Task<Result<List<SectionGroupResponse>>> AllSectionGroups(string userId, Guid sectionId, CancellationToken ct = default);
+        Task<Result<SectionGroupResponse>> AddExerciseToGroup(string userId, Guid groupId, Guid? exerciseId, Guid? customExerciseId, CancellationToken ct = default);
+        Task<Result<Updated>> UpdateGroup(string userId, Guid groupId, string name, string descripion, CancellationToken ct = default);
+        Task<Result<Deleted>> DeleteGroup(string userId, Guid groupId, CancellationToken ct = default);
+        Task<Result<Deleted>> DeleteExerciseFromGroup(string userId, Guid groupId, Guid? exerciseId, Guid? customExerciseId,
+                                                                           CancellationToken ct = default);
         Task<Result<Deleted>> DeleteExerciseAsync(Guid id, CancellationToken ct = default);
         Task<Result<Updated>> UpdateExerciseAsync(Guid id, Guid sectionId, string name, string? description, string? instructions, IFormFile? imageFile, string? equipment, DifficultyLevel? difficultyLevel, int? defaultSets, int? defaultReps, CancellationToken ct = default);
         Task<Result<ExerciseResponse>> CreateExerciseAsync(Guid sectionId, string name, string? description = null, string? instructions = null, IFormFile? imageFile = null, string? equipment = null, DifficultyLevel? difficultyLevel = null, int? defaultSets = null, int? defaultReps = null, CancellationToken ct = default);
-        Task<Result<List<ExerciseResponse>>> GetExercisesBySectionAsync(Guid sectionId, DifficultyLevel? difficulty = null, CancellationToken ct = default);
+        Task<Result<ExercisesResponse>> GetExercisesBySectionAsync(string userId, Guid sectionId, string? searchTerm = null, DifficultyLevel? difficulty = null, CancellationToken ct = default);
         Task<Result<ExerciseResponse>> GetExerciseAsync(Guid exerciseId, CancellationToken ct = default);
-        Task<Result<CustomExerciseRes>> CreateCustomExerciseAsync(string userId, string name, string? description = null, string? Instructions = null, string? Equipment = null, IFormFile? ImageFile = default, CancellationToken ct = default);
-        Task<Result<List<CustomExerciseRes>>> GetCustomExercisesAsync(string userId, CancellationToken ct = default);
+        Task<Result<CustomExerciseRes>> CreateCustomExerciseAsync(string userId, Guid sectionId, string name, string? description = null, string? Instructions = null, string? Equipment = null, IFormFile? ImageFile = default, DifficultyLevel? DifficultyLevel = null, CancellationToken ct = default);
+        Task<Result<List<CustomExerciseRes>>> GetCustomExercisesAsync(string userId, DifficultyLevel? difficulty = null, CancellationToken ct = default);
         Task<Result<CustomExerciseRes>> GetCustomExerciseAsync(string userId, Guid exerciseId, CancellationToken ct = default);
-        Task<Result<Updated>> UpdateCustomExerciseAsync(string userId, Guid exerciseId, string name, string? description = null, string? Instructions = null, string? Equipment = null, IFormFile? imageFile = default, CancellationToken ct = default);
+        Task<Result<Updated>> UpdateCustomExerciseAsync(string userId, Guid exerciseId, Guid sectionId, string name, string? description = null, string? Instructions = null, string? Equipment = null, IFormFile? imageFile = default, DifficultyLevel? DifficultyLevel = null, CancellationToken ct = default);
         Task<Result<Deleted>> DeleteCustomExerciseAsync(string userId, Guid exerciseId, CancellationToken ct = default);
     }
 }
