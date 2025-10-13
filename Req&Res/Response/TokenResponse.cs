03efc4dcffb54d516cf52647c053c5a15cea75bd
abdoshady <abdos@KAKASHI>
2025-10-13 16:37:14 +0300

    feat: extend TokenResponse to include AppUserDto user details

--- Req&Res/Response/TokenResponse.cs
+++ Req&Res/Response/TokenResponse.cs
@@ -1,9 +1,12 @@
-﻿namespace GymAssistant_API.Model.Identity.Dtos
+﻿using GymAssistant_API.Req_Res.Response;
+
+namespace GymAssistant_API.Model.Identity.Dtos
 {
     public class TokenResponse
     {
         public string? AccessToken { get; set; }
         public string? RefreshToken { get; set; }
         public DateTime ExpiresOnUtc { get; set; }
+        public AppUserDto? User { get; set; } = null;
     }
 }
