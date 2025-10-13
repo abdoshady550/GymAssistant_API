0a5467a920f550040b5f2087687b452893ee62e8
abdoshady <abdos@KAKASHI>
2025-10-13 16:34:48 +0300

    feat: add StatesRes record to represent user progress statistics

---
+++ Req&Res/Response/Records/StatesRes.cs
@@ -0,0 +1,15 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Progress
+{
+    public record StatesRes(
+        int? workoutsCount = default,
+        int? customExercisesCount = default,
+        int? recordsCount = default,
+        int? daysSinceLastWorkout = default,
+        int? daysSinceLastRecord = default,
+        double? averageWorkoutDuration = default,
+        decimal? averageWorkoutsPerWeek = default,
+        decimal? personalBestWeight = default,
+        int? personalBestReps = default,
+        decimal? personalBestVolume = default
+        );
+}
