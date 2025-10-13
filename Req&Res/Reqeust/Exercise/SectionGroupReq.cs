ea7ae05488a17da3738f8383692af3aeba4a783d
abdoshady <abdos@KAKASHI>
2025-10-13 16:27:26 +0300

    Add SectionGroupReq record to new Exercise namespace
    A new namespace `GymAssistant_API.Req_Res.Reqeust.Exercise` was introduced. Within this namespace, a new record type `SectionGroupReq` was added. The record includes three properties: `SectionId` (Guid), `Name` (string), and an optional `Description` (string?) with a default value of `null`.

---
+++ Req&Res/Reqeust/Exercise/SectionGroupReq.cs
@@ -0,0 +1,10 @@
+﻿namespace GymAssistant_API.Req_Res.Reqeust.Exercise
+{
+    public record SectionGroupReq(
+        Guid SectionId,
+        string Name,
+        string? Description = null
+        )
+    {
+    }
+}
