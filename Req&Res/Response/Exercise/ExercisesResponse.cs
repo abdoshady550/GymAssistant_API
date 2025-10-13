9a0a6bd6d9be4369fab4463b440fb4440c794a03
abdoshady <abdos@KAKASHI>
2025-10-13 16:33:23 +0300

    feat: create ExercisesResponse record for grouped exercise data

---
+++ Req&Res/Response/Exercise/ExercisesResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace GymAssistant_API.Req_Res.Response.Exercise
+{
+    public record ExercisesResponse(
+        List<ExerciseResponse> Exercise
+        , List<CustomExerciseRes> CustomExercise
+        );
+}
