4d058f45e4638c532f2ac72e4af50a1638cf1812
abdoshady <abdos@KAKASHI>
2025-10-22 01:07:28 +0300

    add notification Conig

---
+++ Data/Configurations/Notification/DeviceTokenConfig.cs
@@ -0,0 +1,42 @@
+﻿using GymAssistant_API.Model.Entities;
+using GymAssistant_API.Model.Entities.Notifications;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymAssistant_API.Data.Configurations.Notifications
+{
+    public class DeviceTokenConfig : IEntityTypeConfiguration<DeviceToken>
+    {
+        public void Configure(EntityTypeBuilder<DeviceToken> builder)
+        {
+            builder.ToTable("DeviceTokens");
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.UserId)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(e => e.Token)
+                .IsRequired()
+                .HasMaxLength(500);
+
+            builder.Property(e => e.Platform)
+                .IsRequired()
+                .HasConversion<int>();
+
+            builder.Property(e => e.IsActive)
+                .IsRequired();
+
+            builder.Property(e => e.LastUsedUtc)
+                .IsRequired();
+
+            builder.Property(e => e.CreatedAtUtc)
+                .IsRequired();
+
+            // Indexes
+            builder.HasIndex(e => e.UserId);
+            builder.HasIndex(e => e.Token);
+            builder.HasIndex(e => new { e.UserId, e.Token }).IsUnique();
+        }
+    }
+}
---
+++ Data/Configurations/Notification/NotificationConfig.cs
@@ -0,0 +1,50 @@
+﻿using GymAssistant_API.Model.Entities;
+using GymAssistant_API.Model.Entities.Exercise;
+using GymAssistant_API.Model.Entities.Notifications;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GymAssistant_API.Data.Configurations.Notifications
+{
+    public class NotificationConfig : IEntityTypeConfiguration<Notification>
+    {
+        public void Configure(EntityTypeBuilder<Notification> builder)
+        {
+            builder.ToTable("Notifications");
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.UserId)
+                .IsRequired()
+                .HasMaxLength(450);
+
+            builder.Property(e => e.Title)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(e => e.Body)
+                .IsRequired()
+                .HasMaxLength(1000);
+
+            builder.Property(e => e.Type)
+                .IsRequired()
+                .HasConversion<int>();
+
+            builder.Property(e => e.Data)
+                .HasMaxLength(2000);
+
+            builder.Property(e => e.IsRead)
+                .IsRequired();
+
+            builder.Property(e => e.IsSent)
+                .IsRequired();
+
+            builder.Property(e => e.CreatedAtUtc)
+                .IsRequired();
+
+            // Indexes
+            builder.HasIndex(e => e.UserId);
+            builder.HasIndex(e => new { e.UserId, e.IsRead });
+            builder.HasIndex(e => e.CreatedAtUtc);
+        }
+    }
+}
