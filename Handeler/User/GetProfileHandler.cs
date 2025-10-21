652118cdc2187a8f77e60f7ef845315b084d8265
abdoshady <abdos@KAKASHI>
2025-10-22 01:24:59 +0300

    refactor(user): simplify GetProfileHandler by removing pagination parameters from Handle method

--- Handeler/User/GetProfileHandler.cs
+++ Handeler/User/GetProfileHandler.cs
@@ -10,9 +10,9 @@ namespace GymAssistant_API.Handeler.User
     {
         private readonly ILogger<GetProfileHandler> logger = logger;
         private readonly IProfile profile = profile;
-        public async Task<Result<ProfileResponse>> Handle(string Id, int pageSize, int page, CancellationToken ct = default)
+        public async Task<Result<ProfileResponse>> Handle(string Id, CancellationToken ct = default)
         {
-            var getProfile = await profile.GetProfileAsync(Id, pageSize, page, ct);
+            var getProfile = await profile.GetProfileAsync(Id, ct);
             if (getProfile.IsError)
             {
                 logger.LogError("Failed to get profile for user {UserId}: {Error}", Id, getProfile.Errors);
