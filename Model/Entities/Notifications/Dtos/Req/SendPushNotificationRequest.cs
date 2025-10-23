03735c653505e56a7ce654caddaba061f6548e96
abdoshady <abdos@KAKASHI>
2025-10-23 14:13:18 +0300

    feat(notification): add SendPushNotificationRequest DTO for individual push notification requests

---
+++ Model/Entities/Notifications/Dtos/Req/SendPushNotificationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymAssistant_API.Model.Entities.Notifications.Dtos.Req
+{
+    /// <summary>
+    /// Request to send push notification
+    /// </summary>
+    public record SendPushNotificationRequest(
+        [Required] string UserId,
+        [Required] string Title,
+        [Required] string Body,
+        NotificationType Type = NotificationType.General,
+        Dictionary<string, string>? Data = null,
+        IFormFile? Image = null
+    );
+}
