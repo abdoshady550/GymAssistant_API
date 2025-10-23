a6c00ee2bb95d5f5280c37898ed06636c2515fd1
abdoshady <abdos@KAKASHI>
2025-10-23 14:12:45 +0300

    feat(notification): add RegisterDeviceTokenRequest DTO with validation attributes for device registration

---
+++ Model/Entities/Notifications/Dtos/Req/RegisterDeviceTokenRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymAssistant_API.Model.Entities.Notifications.Dtos.Req
+{
+    /// <summary>
+    /// Request to register device token
+    /// </summary>
+    public record RegisterDeviceTokenRequest(
+        [Required] string Token,
+        [Required] DevicePlatform Platform
+    );
+}
