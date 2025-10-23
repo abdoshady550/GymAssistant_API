f8dad20c97a6b764cca8f8e092c17ed09714e79d
abdoshady <abdos@KAKASHI>
2025-10-23 14:13:36 +0300

    feat(notification): add SendToTokenPushNotificationRequest DTO for sending notifications directly to a device token

---
+++ Model/Entities/Notifications/Dtos/Req/SendToTokenPushNotificationRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymAssistant_API.Model.Entities.Notifications.Dtos.Req
+{
+    /// <summary>
+    /// Request to send push notification
+    /// </summary>
+    public record SendToTokenPushNotificationRequest(
+        [Required] string token,
+        [Required] string title,
+        [Required] string body,
+        Dictionary<string, string>? data = null,
+        IFormFile? image = null
+    );
+}
