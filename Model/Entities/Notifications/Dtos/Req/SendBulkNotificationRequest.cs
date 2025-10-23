51818ea104bb559be534dc6d3746ec6e0fda188e
abdoshady <abdos@KAKASHI>
2025-10-23 14:13:00 +0300

    feat(notification): add SendBulkNotificationRequest DTO for sending notifications to multiple users

---
+++ Model/Entities/Notifications/Dtos/Req/SendBulkNotificationRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GymAssistant_API.Model.Entities.Notifications.Dtos.Req
+{
+    /// <summary>
+    /// Request to send notification to multiple users
+    /// </summary>
+    public record SendBulkNotificationRequest(
+        [Required] List<string> UserIds,
+        [Required] string Title,
+        [Required] string Body,
+        NotificationType Type = NotificationType.General,
+        Dictionary<string, string>? Data = null,
+        IFormFile? Image = null
+    );
+}
