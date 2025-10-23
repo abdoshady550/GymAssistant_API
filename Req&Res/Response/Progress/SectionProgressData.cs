e813a8a39b0489247c9b17442cba4f5b14f0a611
abdoshady <abdos@KAKASHI>
2025-10-23 14:45:28 +0300

    feat(progress): support nullable and custom exercise names in SectionProgressData

--- Req&Res/Response/Progress/SectionProgressData.cs
+++ Req&Res/Response/Progress/SectionProgressData.cs
@@ -2,8 +2,10 @@
 {
     public class SectionProgressData
     {
-        public Guid ExerciseId { get; set; }
-        public string ExerciseName { get; set; } = string.Empty;
+        public Guid? ExerciseId { get; set; } = null;
+        public string? ExerciseName { get; set; } = null;
+        public Guid? customExerciseId { get; set; } = null;
+        public string? customExerciseName { get; set; } = null;
         public decimal TotalVolume { get; set; }
         public int TotalSets { get; set; }
         public decimal MaxWeight { get; set; }
