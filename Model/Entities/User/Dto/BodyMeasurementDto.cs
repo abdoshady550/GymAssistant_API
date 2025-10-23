79819fbcdb9e1d7b27af338ca7db161b0a99b558
abdoshady <abdos@KAKASHI>
2025-10-23 14:19:12 +0300

    refactor(user): extend BodyMeasurementDto to include goal metrics for weight, muscle mass, and body

--- Model/Entities/User/Dto/BodyMeasurementDto.cs
+++ Model/Entities/User/Dto/BodyMeasurementDto.cs
@@ -3,8 +3,11 @@
     public record BodyMeasurementDto(
      Guid? Id = default,
      decimal? WeightKg = default,
+     decimal? WeightGoal = default,
      decimal? MuscleMassKg = default,
+     decimal? MuscleMassGoal = default,
      decimal? BodyFatPercent = default,
+     decimal? BodyFatGoal = default,
      DateTimeOffset? CreatedAtUtc = default);
 
 }
