d38f73b525871c756fbf1c23e0e919d64ba98af5
abdoshady <abdos@KAKASHI>
2025-10-23 14:38:27 +0300

    feat(user): add ChangePasswordRequest and ChangeSeedingPasswordRequest records for password management

---
+++ Req&Res/Reqeust/User/ChangePasswordRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace GymAssistant_API.Req_Res.Reqeust.User
+{
+    public record ChangePasswordRequest(
+        string CurrentPassword,
+        string NewPassword,
+        string ConfirmPassword
+        );
+    public record ChangeSeedingPasswordRequest(
+    string NewPassword
+    );
+}
