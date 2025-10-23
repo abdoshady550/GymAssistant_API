85cd406fa6334c064ceb039b361ade19c38b16fa
abdoshady <abdos@KAKASHI>
2025-10-23 14:16:15 +0300

    feat(notification): add NotificationResponse DTO for returning notification details

---
+++ Model/Entities/Notifications/Dtos/Res/NotificationResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace GymAssistant_API.Model.Entities.Notifications.Dtos.Res
+{
+
+    /// <summary>
+    /// Notification response
+    /// </summary>
+    public record NotificationResponse(
+        Guid Id,
+        string Title,
+        string Body,
+        NotificationType Type,
+        bool IsRead,
+        DateTimeOffset CreatedAtUtc,
+        DateTimeOffset? ReadAtUtc,
+        Dictionary<string, string>? Data = null,
+        string? Image = null
+    );
+}
