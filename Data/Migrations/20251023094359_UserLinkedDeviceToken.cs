b9b2853ee4c5ec2cf5aef678de931f772b5d7a2f
abdoshady <abdos@KAKASHI>
2025-10-23 14:01:38 +0300

    UserLinkedDeviceToken

---
+++ Data/Migrations/20251023094359_UserLinkedDeviceToken.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAssistant_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserLinkedDeviceToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddForeignKey(
+                name: "FK_DeviceTokens_AppUsers_UserId",
+                table: "DeviceTokens",
+                column: "UserId",
+                principalTable: "AppUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DeviceTokens_AppUsers_UserId",
+                table: "DeviceTokens");
+        }
+    }
+}
