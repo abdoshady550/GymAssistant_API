5ee8885633d761a0ccd8fd7bc17569c6dc8ecca9
abdoshady <abdos@KAKASHI>
2025-10-25 23:45:44 +0300

    edit traineeId

--- Repository/Interfaces/User/Trainer/ITrainerRequestService.cs
+++ Repository/Interfaces/User/Trainer/ITrainerRequestService.cs
@@ -1,17 +1,16 @@
-﻿using GymAssistant_API.Model.Results;
+﻿using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
 using GymAssistant_API.Req_Res.Response.Trainer;
 
 namespace GymAssistant_API.Repository.Interfaces.User.Trainer
 {
     public interface ITrainerRequestService
     {
-        // Trainer actions
         Task<Result<TrainerRequestResponse>> SendRequestAsync(
             string trainerId,
-            Guid traineeId,
+            string traineeId,
             string? message = null,
             CancellationToken ct = default);
-
         Task<Result<TrainerRequestListResponse>> GetSentRequestsAsync(
             string trainerId,
             int pageSize,
@@ -22,28 +21,34 @@ namespace GymAssistant_API.Repository.Interfaces.User.Trainer
             string trainerId,
             Guid requestId,
             CancellationToken ct = default);
-
-        // Trainee actions
         Task<Result<TrainerRequestListResponse>> GetReceivedRequestsAsync(
-            string traineeId,
+            string trainerId,
             int pageSize,
             int pageNumber,
             CancellationToken ct = default);
 
         Task<Result<TrainerRequestResponse>> AcceptRequestAsync(
-            string traineeId,
+            string trainerId,
             Guid requestId,
             CancellationToken ct = default);
 
         Task<Result<TrainerRequestResponse>> RejectRequestAsync(
-            string traineeId,
+            string trainerId,
             Guid requestId,
             CancellationToken ct = default);
-
-        // Common
         Task<Result<TrainerRequestResponse>> GetRequestByIdAsync(
             string userId,
             Guid requestId,
             CancellationToken ct = default);
+        Task<Result<List<UserDto>>> GetAllUserAsync(
+          string? searchTerm,
+          int pageSize,
+          int pageNumber,
+          CancellationToken ct = default);
+        Task<Result<List<UserDto>>> GetAllTrainerAsync(
+          string? searchTerm,
+          int pageSize,
+          int pageNumber,
+          CancellationToken ct = default);
     }
 }
