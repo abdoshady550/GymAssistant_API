c67816168fd8877521b32a966eb2213b3c7260f6
abdoshady <abdos@KAKASHI>
2025-10-25 23:47:11 +0300

    add IUserRequestService

---
+++ Repository/Interfaces/User/Trainer/IUserRequestService.cs
@@ -0,0 +1,57 @@
+﻿using GymAssistant_API.Model.Identity.Dtos;
+using GymAssistant_API.Model.Results;
+using GymAssistant_API.Req_Res.Response.Trainer;
+
+namespace GymAssistant_API.Repository.Interfaces.User.Trainer
+{
+    public interface IUserRequestService
+    {
+        Task<Result<TrainerRequestResponse>> SendRequestAsync(
+            string trainerId,
+            string traineeId,
+            string? message = null,
+            CancellationToken ct = default);
+
+        Task<Result<TrainerRequestListResponse>> GetSentRequestsAsync(
+            string trainerId,
+            int pageSize,
+            int pageNumber,
+            CancellationToken ct = default);
+
+        Task<Result<Deleted>> CancelRequestAsync(
+            string trainerId,
+            Guid requestId,
+            CancellationToken ct = default);
+
+        Task<Result<TrainerRequestListResponse>> GetReceivedRequestsAsync(
+            string trainerId,
+            int pageSize,
+            int pageNumber,
+            CancellationToken ct = default);
+
+        Task<Result<TrainerRequestResponse>> AcceptRequestAsync(
+            string trainerId,
+            Guid requestId,
+            CancellationToken ct = default);
+
+        Task<Result<TrainerRequestResponse>> RejectRequestAsync(
+            string trainerId,
+            Guid requestId,
+            CancellationToken ct = default);
+
+        Task<Result<TrainerRequestResponse>> GetRequestByIdAsync(
+            string userId,
+            Guid requestId,
+            CancellationToken ct = default);
+        Task<Result<List<UserDto>>> GetAllUserAsync(
+          string? searchTerm,
+          int pageSize,
+          int pageNumber,
+          CancellationToken ct = default);
+        Task<Result<List<UserDto>>> GetAllTrainerAsync(
+          string? searchTerm,
+          int pageSize,
+          int pageNumber,
+          CancellationToken ct = default);
+    }
+}
