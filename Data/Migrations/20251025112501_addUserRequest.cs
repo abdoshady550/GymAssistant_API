a042a00b37c637e8a113eeb7e42f9881156093d0
abdoshady <abdos@KAKASHI>
2025-10-25 23:30:27 +0300

    add migrations

---
+++ Data/Migrations/20251025112501_addUserRequest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GymAssistant_API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUserRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TrainerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TraineeId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RespondedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    CreatedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserRequests_ClientProfiles_TraineeId",
+                        column: x => x.TraineeId,
+                        principalTable: "ClientProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserRequests_ClientProfiles_TrainerId",
+                        column: x => x.TrainerId,
+                        principalTable: "ClientProfiles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRequests_TraineeId",
+                table: "UserRequests",
+                column: "TraineeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRequests_TrainerId",
+                table: "UserRequests",
+                column: "TrainerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRequests");
+        }
+    }
+}
