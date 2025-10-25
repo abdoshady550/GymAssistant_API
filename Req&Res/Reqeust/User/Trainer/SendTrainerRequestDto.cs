8872b67daec7a531b46ed42bced62e3c8258571b
abdoshady <abdos@KAKASHI>
2025-10-25 23:50:32 +0300

    add SendUserRequestDto

--- Req&Res/Reqeust/User/Trainer/SendTrainerRequestDto.cs
+++ Req&Res/Reqeust/User/Trainer/SendTrainerRequestDto.cs
@@ -1,7 +1,11 @@
 ﻿namespace GymAssistant_API.Req_Res.Reqeust.User.Trainer
 {
     public record SendTrainerRequestDto(
-         Guid TraineeId,
+         string TraineeId,
          string? Message = null
      );
+    public record SendUserRequestDto(
+      string TrainerId,
+      string? Message = null
+  );
 }
